7b38f883fbbbf09e95134a3d3aaaf60f726d6b75
LeeJaeHyeok97 <hazardous10@naver.com>
2024-07-23 05:19:47 +0900

    first commit

---
+++ src/main/java/com/aidiary/AiDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.aidiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiDiaryApplication.class, args);
+    }
+
+}
