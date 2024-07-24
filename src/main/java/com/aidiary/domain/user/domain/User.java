df99c6a960f6c4348f0ce0e9dbcf76e0a599d53e
LeeJaeHyeok97 <hazardous10@naver.com>
2024-07-24 23:18:24 +0900

    feat: User 엔터티 생성

--- src/main/java/com/aidiary/domain/user/domain/User.java
+++ src/main/java/com/aidiary/domain/user/domain/User.java
@@ -1,4 +1,40 @@
 package com.aidiary.domain.user.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nickname;
+
+    private String name;
+
+    private String email;
+
+    private String role;
+
+
+    @Builder
+    public User(String nickname, String name, String email, String role) {
+        this.nickname = nickname;
+        this.name = name;
+        this.email = email;
+        this.role = role;
+    }
+
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
