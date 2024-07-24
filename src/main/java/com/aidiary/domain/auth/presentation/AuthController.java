37f096d0af26771955b6e1108763af21558668d2
이재혁 <67510260+LEEJaeHyeok97@users.noreply.github.com>
2024-07-25 03:20:31 +0900

    docs: swagger 태그(Authorization) 추가 (#6)
    * Initial commit
    
    * Feat/#1 oauth2login (#3)
    
    * feat: User 엔터티 생성
    
    * feat: jwt 버전 11->12, JWTUtil 생성
    
    * feat: JWTFilter(JwtAuthenticationFilter) 등록
    
    * feat: kakao 로그인 구현
    
    * docs: swagger 태그(Authorization) 추가 (#5)
    
    * feat: User 엔터티 생성
    
    * feat: jwt 버전 11->12, JWTUtil 생성
    
    * feat: JWTFilter(JwtAuthenticationFilter) 등록
    
    * feat: kakao 로그인 구현
    
    * docs: swagger 태그(Authorization) 추가

--- src/main/java/com/aidiary/domain/auth/presentation/AuthController.java
+++ src/main/java/com/aidiary/domain/auth/presentation/AuthController.java
@@ -14,12 +14,14 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authorization", description = "Authorization API")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
