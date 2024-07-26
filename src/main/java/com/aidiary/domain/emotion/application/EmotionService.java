747f900657605bb635c9514acf42d4626de8f15c
이재혁 <67510260+LEEJaeHyeok97@users.noreply.github.com>
2024-07-26 15:00:41 +0900

    feat: 감정 저장 기능 구현 (#33)
    * feat: 일기 감정 분석 기능 구현
    
    * feat: 감정 저장 기능 구현

--- src/main/java/com/aidiary/domain/emotion/application/EmotionService.java
+++ src/main/java/com/aidiary/domain/emotion/application/EmotionService.java
@@ -5,6 +5,7 @@ import com.aidiary.domain.diary.domain.repository.DiaryRepository;
 import com.aidiary.domain.user.domain.User;
 import com.aidiary.domain.user.domain.repository.UserRepository;
 import com.aidiary.global.config.security.token.UserPrincipal;
+import com.aidiary.global.payload.Message;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -32,5 +33,26 @@ public class EmotionService {
 
         return "";
 
+    }
+
+    public Message saveEmotion(UserPrincipal userPrincipal, Long diaryId, String emotionState) {
+        User user = userRepository.findById(userPrincipal.getId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        Diary diary = diaryRepository.findById(diaryId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        if (diary.getUser().equals(user)) {
+            diary.updateEmotion(emotionState);
+
+            return Message.builder()
+                    .message("감정 저장에 성공했습니다.")
+                    .build();
+        }
+
+
+        return Message.builder()
+                .message("감정 저장에 실패했습니다.")
+                .build();
     }
 }
