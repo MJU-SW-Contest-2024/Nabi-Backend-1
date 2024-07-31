22e6be2b502183404641aff431d44aecc3d1de35
LeeJaeHyeok97 <hazardous10@naver.com>
2024-07-31 21:44:10 +0900

    feat: :sparkles: fcm 토큰 알림 기능 구현

---
+++ src/main/java/com/aidiary/domain/notification/presentation/NotificationController.java
@@ -0,0 +1,35 @@
+package com.aidiary.domain.notification.presentation;
+
+import com.aidiary.domain.notification.application.FcmService;
+import com.aidiary.domain.notification.dto.FcmReq;
+import com.aidiary.global.payload.ResponseCustom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/fcm")
+public class NotificationController {
+
+    private FcmService fcmService;
+
+
+    @PostMapping
+    public ResponseCustom<?> pushMessage(@RequestBody FcmReq fcmReq) throws IOException {
+        System.out.println(fcmReq.targetToken() + " "
+                + fcmReq.title() + " " + fcmReq.body());
+
+        fcmService.sendMessageTo(
+                fcmReq.targetToken(),
+                fcmReq.title(),
+                fcmReq.body()
+        );
+
+        return ResponseCustom.OK("Message is pushed");
+    }
+}
