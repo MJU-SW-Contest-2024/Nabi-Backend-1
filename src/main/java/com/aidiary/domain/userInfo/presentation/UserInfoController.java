007990eae7ca8ea70cb2704c9cd529c1edc91bd9
이재혁 <67510260+LEEJaeHyeok97@users.noreply.github.com>
2024-08-02 00:52:29 +0900

    feat: :sparkles: 유저 정보 조회 기능 구현 (#72)
    * Initial commit
    
    * Feat/#1 oauth2login (#3)
    
    * feat: User 엔터티 생성
    
    * feat: jwt 버전 11->12, JWTUtil 생성
    
    * feat: JWTFilter(JwtAuthenticationFilter) 등록
    
    * feat: kakao 로그인 구현
    
    * docs: swagger 태그(Authorization) 추가 (#5)
    
    * feat: User 엔터티 생성
    
    * feat: jwt 버전 11->12, JWTUtil 생성
    
    * feat: JWTFilter(JwtAuthenticationFilter) 등록
    
    * feat: kakao 로그인 구현
    
    * docs: swagger 태그(Authorization) 추가
    
    * feat: accesstoken 테스트를 위한 test login 생성 (#9)
    
    * feat: User 엔티티에 상속 (#12)
    
    * feat: BaseEntity 생성
    
    * feat: User 엔티티에 상속
    
    * feat: 일기 생성 기능 구현 (#14)
    
    * feat: accesstoken 테스트를 위한 test login 생성
    
    * feat: 일기 생성 기능 구현
    
    * hotfix: ci 에러 수정 (#16)
    
    * feat: accesstoken 테스트를 위한 test login 생성
    
    * feat: 일기 생성 기능 구현
    
    * hotfix: ci 에러 수정
    
    * fix: OIDC 카카오 로그인 nullPointerException 해결
    
    * feat: 닉네임 설정 기능 구현 (#21)
    
    * feat: 일기 수정 기능 구현 (#25)
    
    * feat: 일기에 감정 컬럼 추가
    
    * feat: 일기 수정 기능 구현
    
    * feat: 일기 삭제 기능 구현 (#27)
    
    * feat: 일기에 감정 컬럼 추가
    
    * feat: 일기 수정 기능 구현
    
    * feat: 일기 삭제 기능 구현
    
    * feat: 일기 감정 분석 기능 구현 (#31)
    
    * feat: 감정 저장 기능 구현 (#33)
    
    * feat: 일기 감정 분석 기능 구현
    
    * feat: 감정 저장 기능 구현
    
    * fix: :bug: 감정 저장 안되던 오류 수정 (#35)
    
    * feat: 일기 감정 분석 기능 구현
    
    * feat: 감정 저장 기능 구현
    
    * fix: :bug: 감정 저장 안되던 오류 수정
    
    * hotfix: :ambulance: 서버 꺼짐 현상 해결 (#37)
    
    * feat: 일기 감정 분석 기능 구현
    
    * feat: 감정 저장 기능 구현
    
    * fix: :bug: 감정 저장 안되던 오류 수정
    
    * hotfix: :ambulance: 서버 꺼짐 현상 해결
    
    * feat: :sparkles: 홈 화면 조회 기능 구현 (#41)
    
    * feat: :sparkles: 회원가입 완료 여부 필드 추가 (#44)
    
    * feat: :sparkles: 일기 상세 조회 구현 (#47)
    
    * feat: :sparkles: 기간 별 감정 통계 조회 기능 구현 (#50)
    
    * feat: :sparkles: 일기 내용 검색 기능 구현 (#52)
    
    * feat: :sparkles: 감정 별 일기 조회 (#54)
    
    * feat: :sparkles: 월 별 일기 조회 기능 구현 (#59)
    
    * ci: :zap: workflow 수정 (#61)
    
    * ci: :zap: workflow 수정
    
    * ci: :zap: workflow 수정
    
    * feat: :sparkles: user 엔터티 fcmToken 컬럼 추가, 로그인 시 토큰 최신화 구현 (#63)
    
    * ci: :zap: workflow 수정
    
    * ci: :zap: workflow 수정
    
    * feat: :sparkles: fcm 토큰 알림 기능 구현
    
    * feat: :sparkles: user 엔터티 fcmToken 컬럼 추가, 로그인 시 토큰 최신화 구현
    
    * feat: :sparkles: 북마크 추가/삭제 기능 구현, 일기/홈화면 조회 쿼리문 수정 (#65)
    
    * feat: :sparkles: 북마크 추가 기능 구현
    
    * feat: :sparkles: 북마크 추가/삭제 기능 구현, 일기/홈화면 조회 쿼리문 수정
    
    * feat: :rocket: fcmtoken 등록 api 분리 (#68)
    
    * feat: :sparkles: 유저 정보 조회 기능 구현 (#71)

---
+++ src/main/java/com/aidiary/domain/userInfo/presentation/UserInfoController.java
@@ -0,0 +1,43 @@
+package com.aidiary.domain.userInfo.presentation;
+
+import com.aidiary.domain.userInfo.application.UserInfoService;
+import com.aidiary.domain.userInfo.dto.UserInfoRes;
+import com.aidiary.global.config.security.token.CurrentUser;
+import com.aidiary.global.config.security.token.UserPrincipal;
+import com.aidiary.global.payload.ErrorResponse;
+import com.aidiary.global.payload.Message;
+import com.aidiary.global.payload.ResponseCustom;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "UserInfo",description = "UserInfo API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user-info")
+public class UserInfoController {
+
+    private final UserInfoService userInfoService;
+
+    @Operation(summary = "유저 정보 조회", description = "해당 유저의 정보를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "유저 정보 조회 성공", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = UserInfoRes.class)))}),
+            @ApiResponse(responseCode = "400", description = "유저 정보 조회 실패", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+    })
+    @GetMapping
+    public ResponseCustom<UserInfoRes> findUserInfo(
+            @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal
+    ) {
+        return ResponseCustom.OK(userInfoService.loadUserInfo(userPrincipal));
+    }
+}
