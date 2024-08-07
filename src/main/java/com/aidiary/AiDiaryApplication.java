1bf6b383f0fe3755245ad726d0033b9a4e40b7f6
LeeJaeHyeok97 <hazardous10@naver.com>
2024-08-08 07:50:34 +0900

    fix: :bug: 시간 출력 수정(utc->seoul)

--- src/main/java/com/aidiary/AiDiaryApplication.java
+++ src/main/java/com/aidiary/AiDiaryApplication.java
@@ -1,12 +1,20 @@
 package com.aidiary;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class AiDiaryApplication {
+
+    @PostConstruct
+    void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AiDiaryApplication.class, args);
