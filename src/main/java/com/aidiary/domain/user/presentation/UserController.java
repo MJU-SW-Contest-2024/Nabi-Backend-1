22c9a07ba5043ced4e686f1831b227eb1d7aaa27
LeeJaeHyeok97 <hazardous10@naver.com>
2024-08-13 02:38:09 +0900

    feat: :sparkles: pdf 파일로 일기 가져오기 기능 구현

--- src/main/java/com/aidiary/domain/user/presentation/UserController.java
+++ src/main/java/com/aidiary/domain/user/presentation/UserController.java
@@ -2,10 +2,13 @@ package com.aidiary.domain.user.presentation;
 
 import com.aidiary.domain.user.application.UserService;
 import com.aidiary.domain.userInfo.dto.UserInfoRes;
+import com.aidiary.global.config.security.token.CurrentUser;
+import com.aidiary.global.config.security.token.UserPrincipal;
 import com.aidiary.global.payload.ErrorResponse;
 import com.aidiary.global.payload.Message;
 import com.aidiary.global.payload.ResponseCustom;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -13,9 +16,11 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Tag(name = "User", description = "User API")
 @RestController
@@ -23,6 +28,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    private final UserService userService;
 
+
+    @Operation(summary = "일기 가져오기", description = "PDF 형식의 일기를 내 일기 목록에 추가합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "일기 가져오기 성공", content = {@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = Message.class)))}),
+            @ApiResponse(responseCode = "400", description = "일기 가져오기 실패", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class))}),
+    })
+    @PostMapping("/pdf")
+    public ResponseCustom<Message> addDiaryPdf(
+            @Parameter(description = "Accesstoken을 입력해주세요.", required = true) @CurrentUser UserPrincipal userPrincipal,
+            @Parameter(description = "PDF 파일을 업로드 해주세요.", required = true) @RequestParam("file") MultipartFile file
+    ) {
+        return ResponseCustom.OK(userService.addDiaryContent(userPrincipal.getId(), file));
+    }
 
 }
